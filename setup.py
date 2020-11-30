46a2341a2713de7da762facf92af94a9659654cf
Andrei Fokau <andrei@funnel.io>
2020-11-30 18:51:48 +0100

    Allow openpyxl<3.1; release 1.0.b25.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=[
         'networkx>=2.0,<2.6',
         'numpy',
-        'openpyxl>=2.6.2,<=3.0.3',
+        'openpyxl>=2.6.2,<3.1',
         'python-dateutil',
         'ruamel.yaml',
     ],
